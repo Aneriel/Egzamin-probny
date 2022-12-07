f2a6ddc55673a5a9e136d3493c99376b363e8df5
Aneriel <bartlomiej.kaczmarsk@microsoft.wsei.edu.pl>
2022-12-07 20:13:11 +0100

    gotowe

--- Egzamin próbny/Program.cs
+++ Egzamin próbny/Program.cs
@@ -1,10 +1,37 @@
-﻿namespace Egzamin_próbny
+﻿using System;
+namespace Egzamin_próbny
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("");
+            try
+            {
+                int a = int.Parse(Console.ReadLine());
+                int b = int.Parse(Console.ReadLine());
+                int c = int.Parse(Console.ReadLine());
+                checked
+                {
+                    Console.WriteLine(a+b+c);
+                }
+            }
+            catch (ArgumentException)
+            {
+                Console.WriteLine("argument exception, exit");
+            }
+            catch(FormatException)
+            {
+                Console.WriteLine("format exception, exit");
+            }
+            catch(OverflowException)
+            {
+                Console.WriteLine("overflow exception, exit");
+            }
+            catch
+            {
+                Console.WriteLine("non supported exception, exit");
+            }
+
         }
     }
 }

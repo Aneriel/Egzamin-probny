46829ede5154b3b83236f98cd9b7b53fdfb676b8
Aneriel <bartlomiej.kaczmarsk@microsoft.wsei.edu.pl>
2022-12-07 20:06:06 +0100

    Revert "Wykonane"
    This reverts commit f138b12d09e0aa38787ba33da72fe4263587e7b3.

--- Egzamin próbny/Program.cs
+++ Egzamin próbny/Program.cs
@@ -4,22 +4,7 @@
     {
         static void Main(string[] args)
         {
-            double obw = (a + b + c);
-            double hobw = obw / 2;
-            double area = Math.Sqrt((hobw*(hobw-a)*(hobw-b)*(hobw-c)));
-            if (precision < 2 || precision > 8)
-            {
-                throw new ArgumentOutOfRangeException("wrong arguments");
-            }
-            if (a < 0 || b < 0 || c < 0)
-            {
-                throw new ArgumentOutOfRangeException("wrong arguments");
-            }
-            if (a + b < c || b + a < c || c + b < a)
-            {
-                throw new ArgumentException("object not exist");
-            }
-            return obw;
+            Console.WriteLine("");
         }
     }
 }

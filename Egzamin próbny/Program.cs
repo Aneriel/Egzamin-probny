e83e1629ffc1a0f5468cc3a79c2a82e1c3be7f03
Aneriel <bartlomiej.kaczmarsk@microsoft.wsei.edu.pl>
2022-12-07 20:06:52 +0100

    Gotowe

--- Egzamin próbny/Program.cs
+++ Egzamin próbny/Program.cs
@@ -1,10 +1,19 @@
-﻿namespace Egzamin_próbny
+﻿public static double TriangleArea(int a, int b, int c, int precision = 2)
 {
-    internal class Program
+    double obw = (a + b + c);
+    double hobw = obw / 2;
+    double area = Math.Sqrt((hobw * (hobw - a) * (hobw - b) * (hobw - c)));
+    if (precision < 2 || precision > 8)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("");
-        }
+        throw new ArgumentOutOfRangeException("wrong arguments");
     }
+    if (a < 0 || b < 0 || c < 0)
+    {
+        throw new ArgumentOutOfRangeException("wrong arguments");
+    }
+    if (a + b < c || b + a < c || c + b < a)
+    {
+        throw new ArgumentException("object not exist");
+    }
+    return area;
 }

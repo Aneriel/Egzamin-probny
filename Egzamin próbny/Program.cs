aa754afb9dbe06380396b675c5ae54227fd79401
Aneriel <bartlomiej.kaczmarsk@microsoft.wsei.edu.pl>
2022-12-08 16:26:32 +0100

    123

--- Egzamin próbny/Program.cs
+++ Egzamin próbny/Program.cs
@@ -1,10 +1,35 @@
-﻿namespace Egzamin_próbny
+﻿using System.Xml;
+
+namespace Egzamin_próbny
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("");
+             
+            int widthStat = int.Parse(Console.ReadLine());
+            if(widthStat %2 == 0)
+            {
+                widthStat -= 1;
+            }
+            int width = widthStat;
+            for(int x =widthStat; x> 0; x--)
+            {
+                
+                Console.WriteLine();
+                for (int i = x; x > 0; i--)
+                {
+                    if (width <= x)
+                    {
+
+                        Console.Write(" ");
+                    }
+                    else
+                    {
+                        Console.Write("*");
+                    }
+                }
+            }
         }
     }
 }
